15ca1097b5cd6189855c3847f48f36a351d8e20a
Zhang Erning <zhang@erning.com>
2025-02-14 06:15:58 +0800

    add vmfusion

---
+++ hosts/vmfusion/configuration.nix
@@ -0,0 +1,34 @@
+{ pkgs, inputs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  virtualisation.vmware.guest.enable = true;
+
+  users.users.erning = {
+    isSystemUser = true;
+    uid = 501; # same as macOS
+    home = "/home/erning";
+    homeMode = "700";
+    group = "users";
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ];
+    createHome = true;
+    shell = pkgs.fish;
+
+    openssh.authorizedKeys.keyFiles = [
+      "${inputs.secrets}/ssh/erning/dragon/id_ed25519.pub"
+      "${inputs.secrets}/ssh/erning/phoenix/id_ed25519.pub"
+      "${inputs.secrets}/ssh/erning/blink/id_ed25519.pub"
+      "${inputs.secrets}/ssh/erning/pineapple/id_ed25519.pub"
+      "${inputs.secrets}/ssh/erning/mango/id_ed25519.pub"
+      "${inputs.secrets}/ssh/erning/vm/id_ed25519.pub"
+    ];
+  };
+}
---
+++ hosts/vmfusion/home.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  inputs,
+  settings,
+  ...
+}:
+
+let
+  ssh-key =
+    {
+      host ? settings.host,
+      user ? settings.user,
+      name ? "id_ed25519",
+    }:
+
+    let
+      file = "ssh/${user}/${host}/${name}";
+    in
+    {
+      age.secrets."${file}" = {
+        file = "${inputs.secrets}/${file}.age";
+        path = "${config.home.homeDirectory}/.ssh/${name}";
+        mode = "600";
+      };
+      home.file.".ssh/${name}.pub".source = "${inputs.secrets}/${file}.pub";
+    };
+in
+{
+  config = {
+    features = {
+      fonts.enable = true;
+
+      tmux.enable = true;
+      neovim.enable = true;
+
+      build-essential.enable = true;
+      nix-support.enable = true;
+      rustup.enable = true;
+      zig.enable = true;
+    };
+  } // (ssh-key { });
+}
