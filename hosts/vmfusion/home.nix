4af80ddde344f2e1b953578174f3f87481978183
Zhang Erning <zhang@erning.com>
2025-02-16 02:12:59 +0800

    features for vmfusion

--- hosts/vmfusion/home.nix
+++ hosts/vmfusion/home.nix
@@ -4,12 +4,12 @@ let
   features = import "${inputs.self}/lib/features.nix";
 in
 {
-  features = features.develop;
-
   age.secrets."ssh/vm/id_ed25519" = {
     file = "${inputs.secrets}/ssh/vm/id_ed25519.age";
     path = "${config.home.homeDirectory}/.ssh/id_ed25519";
     mode = "600";
   };
   home.file.".ssh/id_ed25519.pub".source = "${inputs.secrets}/ssh/vm/id_ed25519.pub";
+
+  features = features.console // features.desktop // features.develop;
 }
