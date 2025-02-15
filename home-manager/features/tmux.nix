0c92036578821d142f145a4ab10d7126e885e9bd
Zhang Erning <zhang@erning.com>
2025-02-16 07:20:27 +0800

    feat(dotfiles): catppuccin tmux; remove cpu/battery modules; zed font-size

--- home-manager/features/tmux.nix
+++ home-manager/features/tmux.nix
@@ -7,6 +7,12 @@
 
 let
   cfg = config.features.tmux;
+  catppuccin-tmux = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "tmux";
+    tag = "v2.1.2";
+    sha256 = "sha256-vBYBvZrMGLpMU059a+Z4SEekWdQD0GrDqBQyqfkEHPg=";
+  };
 in
 {
   options.features.tmux.enable = lib.mkEnableOption "tmux";
@@ -34,22 +40,33 @@ in
         bind -r C-h resize-pane -L 5
         bind -r C-j resize-pane -D 5
         bind -r C-k resize-pane -U 5
-        bind -r C-l resize-pane -R 5
 
-        if-shell "test -e ~/.config/tmux/tmux.conf.local" "source-file ~/.config/tmux/tmux.conf.local"
+        #
+        # catppuccin with cpu battery
+        #
+
+        # Configure the catppuccin plugin
+        set -g @catppuccin_flavor "mocha"
+        set -g @catppuccin_window_status_style "rounded"
+        set -g @catppuccin_date_time_text ' %H:%M'
+
+        # Load catppuccin
+        run ${catppuccin-tmux}/catppuccin.tmux
+
+        # Make the status line pretty and add some modules
+        set -g status-right-length 100
+        set -g status-left-length 100
+        set -g status-left ""
+        set -g status-right "#{E:@catppuccin_status_application}"
+        set -ag status-right "#{E:@catppuccin_status_session}"
+        set -agF status-right "#{E:@catppuccin_status_date_time}"
+
+        #
+        # local config
+        #
+
+        if-shell "test -e ~/.config/tmux/tmux.local.conf" "source-file ~/.config/tmux/tmux.local.conf"
       '';
-      plugins = with pkgs; [
-        {
-          plugin = tmuxPlugins.dracula;
-          extraConfig = ''
-            set -g @dracula-show-powerline true
-            set -g @dracula-show-left-icon session
-            set -g @dracula-show-timezone false
-            set -g @dracula-time-format "%R"
-            set -g @dracula-plugins "cpu-usage battery time"
-          '';
-        }
-      ];
     };
   };
 }
