342bc50a4ba854d7442ebad9fc41c9dd21541354
Zhang Erning <zhang@erning.com>
2025-05-25 22:13:00 +0800

    prepare pomelo

---
+++ hosts/pomelo/home.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  inputs,
+  settings,
+  ...
+}:
+
+let
+  features = import "${inputs.self}/lib/features.nix" { inherit lib; };
+  ssh-key = (import "${inputs.self}/lib/ssh-key.nix" { inherit config inputs; }) settings.host;
+in
+{
+  imports = [
+    (ssh-key "id_ed25519")
+  ];
+
+  features = lib.mkMerge [
+    features.console
+    features.desktop
+    features.develop
+  ];
+
+  # home.packages = with pkgs; [
+  #   ffmpeg
+  #   yt-dlp
+  # ];
+}
