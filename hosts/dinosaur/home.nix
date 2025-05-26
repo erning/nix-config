3dcf32907ba7384c46790234730c88793cd2c281
Zhang Erning <zhang@erning.com>
2025-05-26 11:16:53 +0800

    feat(hosts): flesh out dinosaur and pomelo with pinyin, flatpak, xrdp, and linuxbrew
    Adds pinyin input method; adds dinosaur and pomelo hosts; disables
    suspend and switches to flatpak; enables xrdp on pomelo; wires
    linuxbrew on dinosaur; adds machine descriptions.

--- hosts/dinosaur/home.nix
+++ hosts/dinosaur/home.nix
@@ -9,6 +9,7 @@
 
 let
   features = import "${inputs.self}/lib/features.nix" { inherit lib; };
+  # ssh-key = (import "${inputs.self}/lib/ssh-key.nix" { inherit config inputs; }) settings.host;
   ssh-key =
     let
       host = settings.host;
@@ -39,7 +40,29 @@ in
   ];
 
   home.packages = with pkgs; [
+    ghostty
     ffmpeg
     yt-dlp
+  ];
+
+  home.sessionVariables = {
+    HOMEBREW_PREFIX = "/home/linuxbrew/.linuxbrew";
+
+    # HOMEBREW_VERBOSE = 1
+    HOMEBREW_NO_ANALYTICS = 1;
+    HOMEBREW_NO_AUTO_UPDATE = 1;
+    HOMEBREW_AUTO_UPDATE_SECS = 86400;
+    HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK = 1;
+    HOMEBREW_CURL_RETRIES = 3;
+
+    HOMEBREW_MIRROR_PREFIX = "https://mirrors.ustc.edu.cn";
+    HOMEBREW_BOTTLE_DOMAIN = "${config.home.sessionVariables.HOMEBREW_MIRROR_PREFIX}/homebrew-bottles";
+    # HOMEBREW_BREW_GIT_REMOTE = "$HOMEBREW_MIRROR_PREFIX/brew.git"
+    # HOMEBREW_CORE_GIT_REMOTE = "$HOMEBREW_MIRROR_PREFIX/homebrew-core.git"
+  };
+
+  home.sessionPath = [
+    "$HOMEBREW_PREFIX/bin"
+    "$HOMEBREW_PREFIX/sbin"
   ];
 }
