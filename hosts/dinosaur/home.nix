7463f78df8417b1fa130e2e2ee369b4097caaf63
Zhang Erning <zhang@erning.com>
2025-05-26 16:14:23 +0800

    homebrew (linuxbrew) for dinosaur

--- hosts/dinosaur/home.nix
+++ hosts/dinosaur/home.nix
@@ -44,4 +44,25 @@ in
     ffmpeg
     yt-dlp
   ];
+
+  home.sessionVariables = {
+    HOMEBREW_PREFIX = "/home/linuxbrew/.linuxbrew";
+
+    # HOMEBREW_VERBOSE = 1
+    HOMEBREW_NO_ANALYTICS = 1;
+    HOMEBREW_NO_AUTO_UPDATE = 1;
+    HOMEBREW_AUTO_UPDATE_SECS = 86400;
+    HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK = 1;
+    HOMEBREW_CURL_RETRIES = 3;
+
+    HOMEBREW_MIRROR_PREFIX = "https://mirrors.ustc.edu.cn";
+    HOMEBREW_BOTTLE_DOMAIN = "${config.home.sessionVariables.HOMEBREW_MIRROR_PREFIX}/homebrew-bottles";
+    # HOMEBREW_BREW_GIT_REMOTE = "$HOMEBREW_MIRROR_PREFIX/brew.git"
+    # HOMEBREW_CORE_GIT_REMOTE = "$HOMEBREW_MIRROR_PREFIX/homebrew-core.git"
+  };
+
+  home.sessionPath = [
+    "$HOMEBREW_PREFIX/bin"
+    "$HOMEBREW_PREFIX/sbin"
+  ];
 }
