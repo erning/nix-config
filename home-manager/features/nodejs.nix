e9b80ca1187f7234ae78c603c5bcfc45c7d91122
Zhang Erning <zhang@erning.com>
2025-07-02 14:00:03 +0800

    feat(develop): add nodejs to develop feature

---
+++ home-manager/features/nodejs.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.features.nodejs;
+in
+{
+  options.features.nodejs.enable = lib.mkEnableOption "nodejs";
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      nodejs
+    ];
+  };
+}
