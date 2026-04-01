5bd846c3216128bb882206ac864f8c06a4bbf131
Zhang Erning <zhang@erning.com>
2026-04-01 22:40:22 +0800

    refactor: simplify typst with lib.optionals; remove obsolete darwin comment

--- home-manager/features/typst.nix
+++ home-manager/features/typst.nix
@@ -1,15 +1,10 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-let
-  cfg-fonts = config.features.fonts;
-  fonts = with pkgs.unstable; [
+{
+  _description = "Typst document typesetter";
+  home.packages = [ pkgs.typst ] ++ lib.optionals config.features.fonts.enable (with pkgs.unstable; [
     libertinus
     jetbrains-mono
     newcomputermodern
-    # stix-two
-  ];
-in
-{
-  _description = "Typst document typesetter";
-  home.packages = [ pkgs.typst ] ++ (if cfg-fonts.enable then fonts else [ ]);
+  ]);
 }
