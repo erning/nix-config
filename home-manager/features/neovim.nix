65543bd064ee0cea1ea60ff93e781f96fe43f2b2
Zhang Erning <zhang@erning.com>
2026-04-15 18:00:04 +0800

    refactor(neovim): generate lazyvim wrapper via runCommand

--- home-manager/features/neovim.nix
+++ home-manager/features/neovim.nix
@@ -1,35 +1,34 @@
 { pkgs, config, ... }:
 
 let
-  # Define custom editor wrapper that sets NVIM_APPNAME environment variable
-  # This allows neovim to use a separate config directory (nvim-lazyvim)
-  # while still sharing the same binary. The wrapper is just running nvim
-  # with the NVIM_APPNAME environment variable set.
-  lazyvim = "NVIM_APPNAME=nvim-lazyvim nvim";
+  nvim-multi = pkgs.writeShellScriptBin "nvim-multi" ''
+    case "$(basename "$0")" in
+      lazyvim) export NVIM_APPNAME=nvim-lazyvim ;;
+    esac
+    exec ${pkgs.neovim}/bin/nvim "$@"
+  '';
+
+  nvim-profiles = pkgs.runCommand "nvim-profiles" { } ''
+    mkdir -p $out/bin
+    ln -s ${nvim-multi}/bin/nvim-multi $out/bin/lazyvim
+  '';
 in
 {
   _description = "Neovim editor (LazyVim)";
-  # Install neovim package
-  home.packages = with pkgs; [
-    neovim
+
+  home.packages = [
+    pkgs.neovim
+    nvim-profiles
   ];
 
-  # Symlink LazyVim configuration directory to XDG config
   xdg.configFile = config.lib.dotfiles.configDir "nvim-lazyvim";
 
-  # Symlink lazyvim wrapper script to ~/.local/bin
-  home.file = config.lib.dotfiles.homeFiles [ ".local/bin/lazyvim" ];
-
-  # Set default editor to use the lazyvim wrapper
-  # This makes $EDITOR and $VISUAL use our custom config
   home.sessionVariables = {
-    "EDITOR" = "lazyvim";
-    "VISUAL" = "lazyvim";
+    EDITOR = "lazyvim";
+    VISUAL = "lazyvim";
   };
 
-  # Create shell aliases for 'vi' to use lazyvim
-  # Applies to fish, bash, and zsh shells
-  programs.fish.shellAliases.vi = "${lazyvim}";
-  programs.bash.shellAliases.vi = "${lazyvim}";
-  programs.zsh.shellAliases.vi = "${lazyvim}";
+  programs.fish.shellAliases.vi = "lazyvim";
+  programs.bash.shellAliases.vi = "lazyvim";
+  programs.zsh.shellAliases.vi = "lazyvim";
 }
