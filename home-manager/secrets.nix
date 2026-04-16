f2fcef0c3bf60a3fa68ed9d909b8f157d2c87e8e
Zhang Erning <zhang@erning.com>
2026-04-16 11:34:47 +0800

    feat(secrets): add age/sops tooling and expose public keys

--- home-manager/secrets.nix
+++ home-manager/secrets.nix
@@ -1,9 +1,24 @@
-{ config, inputs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
   imports = [
     inputs.agenix.homeManagerModules.default
   ];
+
+  home.packages = with pkgs; [
+    age
+    ssh-to-age
+  ];
+
+  xdg.configFile."age/pub" = {
+    source = "${inputs.secrets}/age";
+    recursive = true;
+  };
 
   age.identityPaths = [
     "${config.home.homeDirectory}/.config/age/keys.txt"
