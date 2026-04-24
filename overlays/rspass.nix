0d32192a8518f6502ac50954ae57911cd06d2b6a
Zhang Erning <zhang@erning.com>
2026-04-24 17:23:56 +0800

    chore(rspass): bump to 1.0.2

--- overlays/rspass.nix
+++ overlays/rspass.nix
@@ -1,29 +1,27 @@
 final: prev:
 
 let
-  version = "1.0.1";
+  version = "1.0.2";
   system = final.stdenv.hostPlatform.system;
   assets = {
     aarch64-darwin = {
       platform = "macos-aarch64";
-      hash = "sha256-r7yfb/M6A8CUvL0VKymRWDiNiD0M6hxPaoAIV1e06tI=";
+      hash = "sha256-57vUfsDbwh/Za8F30AFsQ64Zxr+IuyfPpkda7S8Du/E=";
     };
     x86_64-darwin = {
       platform = "macos-x86_64";
-      hash = "sha256-AIUj+ODsgwBgNLgpTOhZLPWHU3izxwvBNZU094Okz+0=";
+      hash = "sha256-QzzJ9zw+ZVYEUs2BDe8i7si6+ATdJJxorTWcvBf9nXA=";
     };
     aarch64-linux = {
       platform = "linux-aarch64";
-      hash = "sha256-ZjIZrvf7OUvgjyfGKUPOuJiCWhWCkxdE5U1HW0jupxE=";
+      hash = "sha256-ZJo+IxxcSXpQRIzYHxCU8dg8Z7nNI9Nuie87BcXwI9w=";
     };
     x86_64-linux = {
       platform = "linux-x86_64";
-      hash = "sha256-CXJLEC8sknbPvHTfOklzjwN9NN46pbrw5H0h1MlcwYQ=";
+      hash = "sha256-5GdTMjAKSqpvAcrBqOkQ+hmywaCcU3XoCDzdT5t++UA=";
     };
   };
-  asset =
-    assets.${system}
-      or (throw "rspass: unsupported platform ${system}");
+  asset = assets.${system} or (throw "rspass: unsupported platform ${system}");
 in
 {
   rspass = final.stdenv.mkDerivation {
