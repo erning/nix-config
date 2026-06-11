6dd28c7c77cf73c64ffe5b7dda1479fb6faae75c
Zhang Erning <zhang@erning.com>
2026-06-11 12:49:38 +0800

    feat(vim): add sudo write shortcut
    Add :w!! command-line abbreviations to Vim and LazyVim so readonly
    files can be written through sudo tee without leaving the editor. The
    abbreviation only expands when the full command line is w!!.

--- dotfiles/.config/nvim-lazyvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim-lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Save readonly files with :w!! via sudo.
+vim.cmd([[cnoreabbrev <expr> w!! getcmdtype() ==# ':' && getcmdline() ==# 'w!!' ? 'w !sudo tee % >/dev/null' : 'w!!']])
